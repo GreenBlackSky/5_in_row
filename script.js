b24c3862d038e8373b135ea4a632956324f42e3f
GreenBlackSky <greenblacksky@gmail.com>
2019-09-18 19:50:43 +0300

    preparing chips and focus

--- script.js
+++ script.js
@@ -36,26 +36,6 @@ function shuffle(array) {
 }
 
 
-class Cursor {
-    constructor() {
-
-    }
-    draw() {
-        // TODO implement, animate
-        // TODO change color on lock
-    }
-}
-
-class Chip {
-    constructor(color) {
-        this.color = color;
-    }
-    draw(){
-        // TODO implement, animate
-    }
-}
-
-
 class Slot {
     constructor(c, r) {
         this.x = config.padding + c*(Slot.width + config.padding);
@@ -63,6 +43,7 @@ class Slot {
 
         // TODO replace with chips
         this.color = null;
+        // this.chip = null;
         this.blocked = false;
         this.focusedOn = false;
     }
@@ -79,16 +60,79 @@ class Slot {
         }
         ctx.fill();
         if(this.focusedOn) {
-            ctx.lineWidth = "10";
+            ctx.lineWidth = config.cursor_width;
             ctx.strokeStyle = config.cursor_color;
             ctx.stroke();
         }
         ctx.closePath();
+        if(this.chip) {
+            this.chip.draw();
+        }
     }
 }
 
 Slot.width = Math.floor((canvas.width - config.padding)/config.columns_n) - config.padding;
 Slot.height = Math.floor((canvas.height - config.padding)/(config.rows_n + 2)) - config.padding;
+
+
+class Chip {
+    constructor(c, r, color) {
+        this.x = config.padding + c*(Slot.width + config.padding) + Slot.width * 0.125;
+        this.y = config.padding + (r + 2)*(Slot.height + config.padding) + Slot.height * 0.125;
+        this.color = color;
+    }
+
+    move() {
+        // TODO animate
+    }
+
+    draw(){
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, Chip.width, Chip.height);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+Chip.width = Math.floor(Slot.width * 0.75);
+Chip.height = Math.floor(Slot.height * 0.75);
+
+
+class Cursor {
+    constructor(c, r) {
+        this.x = config.padding + c*(Slot.width + config.padding);
+        this.y = config.padding + (r + 2)*(Slot.height + config.padding);
+    }
+
+    move() {
+        // TODO implement, animate, move here handling keys and mouse
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.rect(
+            this.x, this.y,
+            Slot.width - config.cursor_width, config.cursor_width
+        );
+        ctx.rect(
+            this.x + Slot.width - config.cursor_width, this.y,
+            config.cursor_width, Slot.height - config.cursor_width
+        );
+        ctx.rect(
+            this.x + config.cursor_width, this.y + Slot.height - config.cursor_width,
+            Slot.width - config.cursor_width, config.cursor_width
+        );
+        ctx.rect(
+            this.x, this.y + config.cursor_width,
+            config.cursor_width, Slot.height - config.cursor_width
+        );
+        ctx.fillStyle = config.cursor_color;
+        ctx.fill();
+        ctx.closePath();
+        // TODO change color on lock
+    }
+}
 
 
 class Game {
@@ -119,6 +163,7 @@ class Game {
                 this.slots[y][x] = new Slot(x, y);
                 if(field[y][x] == 'rnd_color') {
                     this.slots[y][x].color = randomColors.pop()
+                    // this.slots[y][x].chip = new Chip(y, x, randomColors.pop())
                 } else if (field[y][x] == 'blocked') {
                     this.slots[y][x].blocked = true;
                 }
